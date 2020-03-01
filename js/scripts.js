792834389cbcad5b80badd75964b70b72ea985ea
adrianweru <adrianweru@gmail.com>
2020-03-01 16:11:30 +0300

    made changes to the scripts.js file

--- js/scripts.js
+++ js/scripts.js
@@ -45,5 +45,16 @@ $("document").ready(function(){
          Total = Total * this.number;
   }
   else alert("Enter your preferences in the appropriate manner")
-  let delivery= prompt
+  var delivery=prompt("Would you like to be delivered to or come to our restaurant ?")
+  if(delivery === "delivered"){
+    alert("your meal is on your way but you have to pay 350 for the delivery expense")
+    Total = Total + 350;
+    var location=prompt("please enter your residence sir or madam")
+    $("#location").text("Your location is" +location)
+  }
+  else if (delivery === "come to our restaurant"){
+    alert("we are expecting you soon and welcome in advance");
+  }
+  else alert("enter your details correctly")
+  $("#details").text("Your pizza will be ready soon, the total price is " +Total +"Your order is "+this.type +" And your pizza size is " this.size);
 })
