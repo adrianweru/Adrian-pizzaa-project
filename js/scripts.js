82cf78db3ba76ecf6ffd3323be9a08c939f7bbb7
adrianweru <adrianweru@gmail.com>
2020-03-02 07:37:22 +0300

    made changes to thhe following files

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 $("document").ready(function(){
-  function person (name,phone,email){
+  function person (name,email,phone){
     this.name = name;
     this.email = email;
     this.phone = phone;
@@ -35,26 +35,36 @@ $("document").ready(function(){
       Total = Total * this.number;
     }
     else if (this.size === "Medium"){
-         price = 750;
-         Total = price + 400;
-         Total =Total * this.number;
-     }
-     else if(this.size ==="small"){
-         price=450;
-         Total = price + 400;
-         Total = Total * this.number;
-  }
-  else alert("Enter your preferences in the appropriate manner")
-  var delivery=prompt("Would you like to be delivered to or come to our restaurant ?")
-  if(delivery === "delivered"){
-    alert("your meal is on your way but you have to pay 350 for the delivery expense")
-    Total = Total + 350;
-    var location=prompt("please enter your residence sir or madam")
-    $("#location").text("Your location is" +location)
-  }
-  else if (delivery === "come to our restaurant"){
-    alert("we are expecting you soon and welcome in advance");
-  }
-  else alert("enter your details correctly")
-  $("#details").text("Your pizza will be ready soon, the total price is " +Total +"Your order is "+this.type +" And your pizza size is " this.size);
-})
+      price = 750;
+      Total = price + 400;
+      Total =Total * this.number;
+    }
+    else if(this.size ==="small"){
+      price=450;
+      Total = price + 400;
+      Total = Total * this.number;
+    }
+    else alert("Enter your preferences in the appropriate manner")
+    var delivery=prompt("Would you like to be delivered to or come to our restaurant ?")
+    if(delivery === "delivered"){
+      alert("your meal is on your way but you have to pay 350 for the delivery expense")
+      Total = Total + 350;
+      var location=prompt("please enter your residence sir or madam")
+      $("#location").text("Your location is" +location)
+    }
+    else if (delivery === "come to our restaurant"){
+      alert("we are expecting you soon and welcome in advance");
+    }
+    else alert("enter your details correctly")
+    $("#details").text("Your pizza will be ready soon, the total price is " +Total +"Your order is "+this.type +" And your pizza size is " this.size);
+
+    $(".calc").click(function(){
+      let type = $("#type").val()
+      let size= $("#size").val();
+      let toppings = $(".toppings").val();
+      let number = $(".number").val();
+      let delivery = $(".delivery").val();
+      let myOrder = new order(size,toppings,number,delivery,type);
+      myOrder.price()
+    });
+  })
